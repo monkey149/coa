49dad3382ceded95da9847ae493b66a23f9f0155
Shubham Chavda <shubhamhpchavda@gmail.com>
2025-10-26 23:09:25 +0530

    Update exp5.asm

--- exp5.asm
+++ exp5.asm
@@ -1,3 +1,5 @@
+; EXP5. Implementation of Block Exchange using Index Registers.
+
 CODE SEGMENT
 ASSUME CS: CODE
 START: MOV AX, 5000H
@@ -19,3 +21,19 @@ REP MOVSB
 INT 3H
 CODE ENDS
 END START
+
+
+; How to run!!!!!!!!! 
+; mount c (location of your folder)
+; c:
+; cd 8086
+; masm exp5.asm (fir teen baar enter karo)
+; link exp5.obj (fir teen baar enter karo, warning aayegi lekin aage badho)
+; debug exp5.exe
+; F 5000:1000 3000 0
+; F 5000:1000 1007 1 2 3 4 5 6 7 8
+; F 5000:2000 2007 11 12 13 14 15 16 17 18
+; G
+; D 5000:1000 1007
+; D 5000:2000 2007
+
