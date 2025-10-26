332002a47cfc9af961c6619e7789bd2a58dc329e
Shubham Chavda <shubhamhpchavda@gmail.com>
2025-10-26 22:38:06 +0530

    Update exp2.asm

--- exp2.asm
+++ exp2.asm
@@ -1,3 +1,5 @@
+;EXP2 : Aim: Implementation of number conversion ASCII to BCD using MASM
+
 DATA SEGMENT
 ASCII DB 31H, 32H, 33H, 34H, 35H, 36H, 37H, 38H, 39H, 30H
     DB 100 DUP(00H)
@@ -17,3 +19,14 @@ BACK: MOV AL,[BX]
     INT 3H
 CODE ENDS
 END START
+
+
+; How to run!!!!!!!!! 
+; mount c (location of your folder)
+; c:
+; cd 8086
+; masm exp2.asm (fir teen baar enter karo)
+; link exp2.obj (warning aayegi lekin aage badho)
+; TD exp2.exe
+; "Run" click karo ya "f9" (mostly f9 hi karo)
+; view -> another -> dump
