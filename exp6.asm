af84a2a585479a09f7924f219af95aa0fba193f8
Shubham Chavda <shubhamhpchavda@gmail.com>
2025-10-26 23:16:18 +0530

    Update exp6.asm

--- exp6.asm
+++ exp6.asm
@@ -1,3 +1,5 @@
+; EXP6. Implementation of two 8-bit BCD additions
+
 .MODEL SMALL
 .STACK 100H
 .DATA
@@ -16,3 +18,17 @@ MAIN PROC
     INT 21H
 MAIN ENDP
 END MAIN
+
+
+; How to run!!!!!!!!! 
+; mount c (location of your folder)
+; c:
+; cd 8086
+; masm exp6.asm (fir teen baar enter karo)
+; link exp6.obj (fir teen baar enter karo)
+; debug exp6.exe
+; t
+; t
+; t
+; t
+; fir AX = 0763 aaye tab stop ho jao
